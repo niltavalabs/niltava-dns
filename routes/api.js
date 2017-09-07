a95f60a1c5a8c09b7ef6f3f578d17ed60bafa480
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2017-09-07 17:04:32 +0700

    Create SOA and NS record after put domain.

--- routes/api.js
+++ routes/api.js
@@ -40,6 +40,36 @@ router.put('/domain', function(req, res, next) {
   var domain = new Domain(req.body.domain);
   domain.account = req.userId;
   domain.save(function (err, domain, numAffected) {
+    var recordSOAData = {
+      domain: domain.name,
+      name: domain.name,
+      type: 'SOA',
+      value: {
+        ttl: 180000,
+        type: 'SOA',
+        primary: 'ns.mapmu.com.',
+        admin: 'info.mapmu.com.',
+        serial: 2009010351,
+        refresh: 43200,
+        retry: 3600,
+        expiration: 1209600,
+        minimum: 180
+      }
+    }
+    var recordSOA = new Record(recordSOAData);
+    recordSOA.save();
+
+    var recordNSData = {
+      domain: domain.name,
+      name: domain.name,
+      type: 'NS',
+      value: {
+        ttl: 600,
+        data: 'ns.mapmu.com'
+      }
+    }
+    var recordNS = new Record(recordNSData);
+    recordNS.save();
     if (!err) {
       res.status(200).send({msg: 'Success', domain: domain});
     }
@@ -49,7 +79,6 @@ router.put('/domain', function(req, res, next) {
 
 router.get('/domain', function(req, res, next) {
   if(!req.authorized) return res.status(403).send({msg: 'Invalid authorization'});
-
   Domain.findByAccount(req.userId, function(err, domains) {
     res.send({domains: domains});
   });
@@ -59,7 +88,6 @@ router.put('/domain/:domain/record', function(req, res, next) {
   if(!req.authorized) return res.status(403).send({msg: 'Invalid authorization'});
   var record = new Record(req.body.record);
   record.domain = req.params.domain;
-  record.account = req.userId;
   record.save(function (err, record, numAffected) {
     if (!err) {
       res.status(200).send({msg: 'Success', record: record});
